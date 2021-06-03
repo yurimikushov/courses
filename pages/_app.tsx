bae0cbf18b308d3f684c591c77e140e931cb1927
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-03 08:50:08 +0300

    feat: configured root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import './globals.css'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <title>Курсы здесь</title>
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+        rel='stylesheet'
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 )
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from 'next/head'
+import { Heading } from '../components'
 
 const Home = (): JSX.Element => (
-  <div>
-    <Head>
-      <title>Курсы здесь</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+  <>
     <Heading tag='h1'>Home page will be here</Heading>
     <Heading tag='h2'>Home page will be here</Heading>
     <Heading tag='h3'>Home page will be here</Heading>
-  </div>
+  </>
 )
 
 export default Home
