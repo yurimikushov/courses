7a19ea0619ae2cb27f5479b0586a7bcf7724ed7c
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-04 16:30:19 +0300

    chore: added defining svg as react component

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.svg' {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGAElement>>
+  export default content
+}
