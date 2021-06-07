b14552ea0e88443f7801bae65916aa88cf302a89
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-07 08:56:28 +0300

    feat: added app logo

--- src/layouts/Layout/Sidebar/Sidebar.tsx
+++ src/layouts/Layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,19 @@
+import Link from 'next/link'
 import cn from 'classnames'
+import { Menu } from '../Menu/Menu'
+import Logo from '../Logo.svg'
 import { SidebarProps } from './Sidebar.props'
 import styles from './Sidebar.module.css'
-import { Menu } from '../Menu/Menu'
 
 const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => (
   <div className={cn(className, styles.sidebar)} {...props}>
+    <div>
+      <Link href='/'>
+        <a>
+          <Logo />
+        </a>
+      </Link>
+    </div>
     <Menu />
   </div>
 )
