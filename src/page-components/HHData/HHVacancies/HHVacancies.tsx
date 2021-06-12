928dee0f629e4e03848b869cd6dc7d36210f4a31
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-12 21:44:28 +0300

    feat: added formatting total number of vacancies

--- src/page-components/HHData/HHVacancies/HHVacancies.tsx
+++ src/page-components/HHData/HHVacancies/HHVacancies.tsx
@@ -1,10 +1,11 @@
+import { splitToThousands } from '../../../utils'
 import { HHVacanciesProps } from './HHVacancies.props'
 import styles from '../HHData.module.css'
 
 const HHVacancies = ({ title, count }: HHVacanciesProps): JSX.Element => (
   <>
     <div className={styles.title}>{title}</div>
-    <div className={styles.count}>{count}</div>
+    <div className={styles.count}>{splitToThousands(count)}</div>
   </>
 )
 
