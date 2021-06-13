40db7c52ef44b36eb330fb251d3f15103eac2160
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-13 19:29:59 +0300

    refactor: corrected code style

--- src/store/hooks/useMenu.ts
+++ src/store/hooks/useMenu.ts
@@ -1,14 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { IMenuItem } from '../../interfaces'
-import { setMenu as setMenuStore } from '../actions'
+import { setMenu as setMenuAction } from '../actions'
 import { IStore } from '../store'
 
 const useMenu = (): [IMenuItem[], (menu: IMenuItem[]) => void] => {
   const menu: IMenuItem[] = useSelector(({ menu }: IStore) => menu)
 
   const dispatch = useDispatch()
-
-  const setMenu = (menu: IMenuItem[]) => dispatch(setMenuStore(menu))
+  const setMenu = (menu: IMenuItem[]) => dispatch(setMenuAction(menu))
 
   return [menu, setMenu]
 }
