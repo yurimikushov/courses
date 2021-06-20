556c2cb6e716f4da338c95872003e8255861b13a
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-20 11:34:06 +0300

    feat: adapted body of product card

--- src/page-components/Products/Product/Body/Body.tsx
+++ src/page-components/Products/Product/Body/Body.tsx
@@ -13,38 +13,32 @@ const Body = ({
 }: BodyProps): JSX.Element => (
   <div className={cn(className, styles.body)}>
     <div className={styles.description}>{description}</div>
-    <div className={styles.left}>
-      <div>
-        {characteristics.map(({ name, value }) => (
-          <div key={name} className={styles.characteristic}>
-            <span className={styles.characteristicName}>{name}</span>
-            {value && <span className={styles.dots}></span>}
-            {value && (
-              <span className={styles.characteristicValue}>{value}</span>
-            )}
-          </div>
-        ))}
-      </div>
-      <div className={styles.tags}>
-        {tags.map((tag) => (
-          <Label key={tag}>{tag}</Label>
-        ))}
-      </div>
-    </div>
-    <div className={styles.right}>
-      {advantages && (
-        <div className={styles.advantages}>
-          <div className={styles.advantagesTitle}>Преимущества</div>
-          <div>{advantages}</div>
+    <div className={styles.characteristics}>
+      {characteristics.map(({ name, value }) => (
+        <div key={name} className={styles.characteristic}>
+          <span className={styles.characteristicName}>{name}</span>
+          {value && <span className={styles.dots}></span>}
+          {value && <span className={styles.characteristicValue}>{value}</span>}
         </div>
-      )}
-      {disadvantages && (
-        <div className={styles.disadvantages}>
-          <div className={styles.disadvantagesTitle}>Недостатки</div>
-          <div>{disadvantages}</div>
-        </div>
-      )}
+      ))}
     </div>
+    <div className={styles.tags}>
+      {tags.map((tag) => (
+        <Label key={tag}>{tag}</Label>
+      ))}
+    </div>
+    {advantages && (
+      <div className={styles.advantages}>
+        <div className={styles.advantagesTitle}>Преимущества</div>
+        <div>{advantages}</div>
+      </div>
+    )}
+    {disadvantages && (
+      <div className={styles.disadvantages}>
+        <div className={styles.disadvantagesTitle}>Недостатки</div>
+        <div>{disadvantages}</div>
+      </div>
+    )}
   </div>
 )
 
