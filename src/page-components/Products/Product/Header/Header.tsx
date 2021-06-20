d04b8eecf1bfb7f98111c59ea890ebe977bccc27
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-20 17:25:16 +0300

    refactor: restructured layout header of product card

--- src/page-components/Products/Product/Header/Header.tsx
+++ src/page-components/Products/Product/Header/Header.tsx
@@ -19,26 +19,38 @@ const Header = ({
     <div className={styles.logo}>
       <Image src={image} alt='Логотип школы.' width={70} height={70} />
     </div>
-    <Heading className={styles.title} tag='h3'>
-      {title}
-    </Heading>
-    <div className={styles.categories}>
-      {categories.map((category) => (
-        <Label key={category} className={styles.category}>
-          {category}
-        </Label>
-      ))}
+    <div className={styles.titleAndCategoriesBox}>
+      <Heading className={styles.title} tag='h3'>
+        {title}
+      </Heading>
+      <div className={styles.categories}>
+        {categories.map((category) => (
+          <Label key={category} className={styles.category}>
+            {category}
+          </Label>
+        ))}
+      </div>
     </div>
-    <div className={styles.priceValue}>{`${splitToThousands(price)} ₽`}</div>
-    <div className={styles.priceTitle}>цена</div>
-    <div className={styles.creditValue}>
-      {`${splitToThousands(credit)} ₽`}
-      <span className={styles.month}>/мес</span>
+    <div className={styles.priceBox}>
+      <div>
+        <div className={styles.priceValue}>{`${splitToThousands(
+          price
+        )} ₽`}</div>
+        <div className={styles.priceTitle}>цена</div>
+      </div>
+      <div>
+        <div className={styles.creditValue}>
+          {`${splitToThousands(credit)} ₽`}
+          <span className={styles.month}>/мес</span>
+        </div>
+        <div className={styles.creditTitle}>в кредит</div>
+      </div>
     </div>
-    <div className={styles.creditTitle}>в кредит</div>
-    <Rating className={styles.ratingValue} rating={initialRating} />
-    <div className={styles.ratingTitle}>
-      {reviewCount} {declOfNum(reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+    <div className={styles.ratingBox}>
+      <Rating className={styles.ratingValue} rating={initialRating} />
+      <div className={styles.ratingTitle}>
+        {reviewCount} {declOfNum(reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+      </div>
     </div>
   </div>
 )
