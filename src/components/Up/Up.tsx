a38f168b5e5f66c1b11d0b88b692024b7571fc5f
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-29 09:06:34 +0300

    feat: added animation showing/hiding of Up button

--- src/components/Up/Up.tsx
+++ src/components/Up/Up.tsx
@@ -1,5 +1,6 @@
-import cn from 'classnames'
+import { motion } from 'framer-motion'
 import { useScrollY } from '../../hooks'
+import { isBrowser } from '../../utils'
 import UpIcon from './assets/up.svg'
 import styles from './Up.module.css'
 
@@ -13,15 +14,31 @@ const Up = (): JSX.Element => {
     })
   }
 
+  const animationVariants = {
+    visible: {
+      display: 'flex',
+      opacity: 1,
+    },
+    hidden: {
+      transitionEnd: {
+        display: 'none',
+      },
+      opacity: 0,
+    },
+  }
+
+  const needToShow = scrollY > (isBrowser() && window.innerHeight) || 0
+
   return (
-    <div
-      className={cn(styles.up, {
-        [styles.displayed]: scrollY > innerHeight / 2,
-      })}
+    <motion.div
+      className={styles.up}
       onClick={scrollToTop}
+      variants={animationVariants}
+      initial={needToShow ? 'visible' : 'hidden'}
+      animate={needToShow ? 'visible' : 'hidden'}
     >
       <UpIcon />
-    </div>
+    </motion.div>
   )
 }
 
