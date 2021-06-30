d3189e23aef858ef39df05c8bc9f7d6444c674c7
Yuri Mikushov <mikushov.yu@gmail.com>
2021-06-30 09:02:26 +0300

    feat: added mobile header layout

--- src/layouts/Layout/Header/Header.tsx
+++ src/layouts/Layout/Header/Header.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react'
+import Link from 'next/link'
 import cn from 'classnames'
+import Logo from '../Logo.svg'
+import OpenIcon from './assets/open.svg'
+import CloseIcon from './assets/close.svg'
 import { HeaderProps } from './Header.props'
 import styles from './Header.module.css'
 
-const Header = ({ className, ...props }: HeaderProps): JSX.Element => (
-  <header className={cn(className, styles.header)} {...props}>Header</header>
-)
+const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+  const [menuIsOpened, setMenuIsOpened] = useState<boolean>(false)
+
+  return (
+    <header className={cn(className, styles.header)} {...props}>
+      <Link href='/'>
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <button
+        className={styles.menu}
+        onClick={() => setMenuIsOpened((opened) => !opened)}
+      >
+        {menuIsOpened ? <OpenIcon /> : <CloseIcon />}
+      </button>
+    </header>
+  )
+}
 
 export { Header }
