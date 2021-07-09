7d21f455e16ca91dd081c96b3d93cb07de995391
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-09 08:57:36 +0300

    feat: made product card available for screen readers

--- src/page-components/Product/Header/Header.tsx
+++ src/page-components/Product/Header/Header.tsx
@@ -20,10 +20,15 @@ const Header = ({
       <Image src={image} alt='Логотип школы.' width={70} height={70} />
     </div>
     <div className={styles.titleAndCategoriesBox}>
-      <Heading className={styles.title} tag='h3'>
+      <Heading
+        className={styles.title}
+        tag='h3'
+        aria-label={`Название курса '${title}'.`}
+      >
         {title}
       </Heading>
       <div className={styles.categories}>
+        <span className='hidden'>хеш-теги:</span>
         {categories.map((category) => (
           <Label key={category} className={styles.category}>
             {category}
@@ -33,23 +38,38 @@ const Header = ({
     </div>
     <div className={styles.priceBox}>
       <div>
-        <div className={styles.priceValue}>{`${splitToThousands(
-          price
-        )} ₽`}</div>
-        <div className={styles.priceTitle}>цена</div>
+        <div className={styles.priceValue}>
+          <span aria-label={`цена ${price} рублей.`}>
+            {`${splitToThousands(price)} ₽`}
+          </span>
+        </div>
+        <div className={styles.priceTitle} aria-hidden={true}>
+          цена
+        </div>
       </div>
       <div>
         <div className={styles.creditValue}>
-          {`${splitToThousands(credit)} ₽`}
-          <span className={styles.month}>/мес</span>
+          <span aria-label={`в кредит ${credit} рублей в месяц.`}>
+            {`${splitToThousands(credit)} ₽`}
+          </span>
+          <span className={styles.month} aria-hidden={true}>
+            /мес
+          </span>
         </div>
-        <div className={styles.creditTitle}>в кредит</div>
+        <div className={styles.creditTitle} aria-hidden={true}>
+          в кредит
+        </div>
       </div>
     </div>
     <div className={styles.ratingBox}>
+      <span className='hidden'>{`Рейтинг ${initialRating}.`}</span>
       <Rating className={styles.ratingValue} rating={initialRating} />
       <div className={styles.ratingTitle}>
-        {reviewCount} {declOfNum(reviewCount, ['отзыв', 'отзыва', 'отзывов'])}
+        {`${reviewCount} ${declOfNum(reviewCount, [
+          'отзыв',
+          'отзыва',
+          'отзывов',
+        ])}`}
       </div>
     </div>
   </div>
