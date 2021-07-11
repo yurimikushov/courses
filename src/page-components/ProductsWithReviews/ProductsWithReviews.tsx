0111028ec95a1f957968a7acd7a783350805930c
Yuri Mikushov <mikushov.yu@gmail.com>
2021-07-11 10:19:06 +0300

    feat: added control motion of products sorting

--- src/page-components/ProductsWithReviews/ProductsWithReviews.tsx
+++ src/page-components/ProductsWithReviews/ProductsWithReviews.tsx
@@ -1,3 +1,4 @@
+import { useReducedMotion } from 'framer-motion'
 import { ProductsWithReviewsProps } from './ProductsWithReviews.props'
 import { ProductWithReviews } from './ProductWithReviews/ProductWithReviews'
 
@@ -5,17 +6,21 @@ const ProductsWithReviews = ({
   products,
   className,
   ...props
-}: ProductsWithReviewsProps): JSX.Element => (
-  <div className={className} {...props} role='list'>
-    {products.map((product) => (
-      <ProductWithReviews
-        key={product._id}
-        product={product}
-        layout
-        role='listitem'
-      />
-    ))}
-  </div>
-)
+}: ProductsWithReviewsProps): JSX.Element => {
+  const shouldReducedMotion = useReducedMotion()
+
+  return (
+    <div className={className} {...props} role='list'>
+      {products.map((product) => (
+        <ProductWithReviews
+          key={product._id}
+          product={product}
+          layout={shouldReducedMotion === false}
+          role='listitem'
+        />
+      ))}
+    </div>
+  )
+}
 
 export { ProductsWithReviews }
